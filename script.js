76d949871b244a9f77eafdce3a3f9876248553f8
stella <stellawalya27@gmail.com>
2022-11-17 19:20:24 +0300

    changes on style.css.index.html and scipt.js

--- script.js
+++ script.js
@@ -1,88 +1,100 @@
-const Quizdata=[]
-{
-   question:"which language runs in a web browser?"
-   a:"java",
-   b;"c"
-   c:"python"
-   d:"javascript"
-   correct:"d"
- 
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
 
 
-    question:"what does css stand for?",
-    a;"central style sheets"
-    b:"cascading style sheet"
-    c:"cascading simple sheet"
-    d:"cars simple sheet"
-    correct:"d" 
+];
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
 }
-{
-    question:"what does html stand for ?",
-    a;"Hypertext markup language"
-    b:"Hypertext markdown language"
-    c:"Hyperloop machine language"
-    d:"Helicopters Termunals motorboats Lambogins"
-    correct:"a" 
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
 }
-question:"Javascript was launched whith year?"
-    a;"1994"
-    b:"1995"
-    c:"1996"
-    d:"1997"
-    correct:"b"
-{
-}
-    const quiz=document.getElementById('quiz')
-    const answerEls=document.querySelectorAll('.answer')
-    const questionEl=document.getElementById('question')
-    const a_text=document.getElementById('a_text')
-    const b_text=document.getElementById('b_text')
-    const c_text=document.getElementById('c_text')
-    const d_text=document.getElementById('d_text')
-    const submitBtn=document.getElementById('submit')
 
-
-    function loadQuiz() {
-
-        deselectAnswer()
-        
-        const currentquizData=quizData[currentquiz]
-
-        questionEl.innerText=currentquizData.question
-        a_text=innerText=currentquizData.a
-        b_text=innerText=currentquizData.b
-        c_text=innerText=currentquizData.c
-        d_text=innerText=currentquizData.d
-    }
-
-    function getselected() {
-      let answerEls
-      answerEls.forEach(element => {
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
         if(answerEl.checked) {
-            answer=answerEl.id
+            answer = answerEl.id
         }
-        
-      })
-      return answer 
+    })
+    return answer
+}
 
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+       currentQuiz++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
     }
-
-
-
-    submitBtn.addEventListerner('click',() => {
-        const answer=getselected()
-        if(answer){
-            if(answer===quizData[currentquiz].correct) {
-                score++
-            }
-
-            currentquiz++
-
-            if(currentquiz < quiz.length){
-                loadQuiz()
-            }else{
-                quiz.innerHtml= `
-                <h2>you answered${score}/${Quizdata.length}questions correctly</h2>
-
-                <button onclick="location.reload()">Reload>
-            }
+})
